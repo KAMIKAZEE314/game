87ac7a757515e9f7fe9a15fc846e00f180c16739
KAMIKAZEE314 <pinguinos313@gmail.com>
2026-01-30 20:56:31 +0100

    basic things

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
+struct Floor {
+    enemys: vec<Enemy>,
+    loot: Loot,
+    next_floor_loot: vec<Loot>
+}
+
+struct Loot {
+    reward_type: String,
+    reward_amount: i32
+}
+
+struct Enemy {
+    health: u32
+    // ToDo: add some parameters, when building the AI for the enemys
+}
+
 #[allow(unused)]
 fn main() {
-    
+    let floor = 
 }
